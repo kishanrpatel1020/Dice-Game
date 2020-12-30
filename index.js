ccfebc46b72cb2c4883f1abe1cfa99abadb1ee8d
kishanrpatel1020 <69687037+kishanrpatel1020@users.noreply.github.com>
2020-12-29 20:56:54 -0500

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-document.getElementsByTagName("li")[2].innerHTML = "kishan";
-document.getElementsByClassName("btn")[0].style.color = "red";
-document.querySelector("ul a").style.color ="blue";
-document.querySelector("button").style.backgroundColor = "yellow";
-document.querySelector("h1").classList.add("hug");
